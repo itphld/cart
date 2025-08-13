7abe35ab8cd8b90618735a126517824aa7c7b752
itphld <itplanet12@gmail.com>
2025-08-13 17:17:37 +0530

    Add static assets

--- WebShopX/settings.py
+++ WebShopX/settings.py
@@ -129,11 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=BASE_DIR / 'staticfiles'
-STATICFILES_DIRS=[BASE_DIR / 'WebShopX' / 'static',]
+STATIC_ROOT=BASE_DIR /'static'
+STATICFILES_DIRS=[BASE_DIR/'WebShopX'/'static',]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR/ 'media'#STATIC_URL = "static/"
+MEDIA_ROOT = BASE_DIR/'media'#STATIC_URL = "static/"
 
 
 # Default primary key field type
