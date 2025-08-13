05a7dd22b575fafcebf5907e873f216beeceb014
itphld <itplanet12@gmail.com>
2025-08-13 13:38:53 +0530

    Fix SECRET_KEY and static config

--- WebShopX/settings.py
+++ WebShopX/settings.py
@@ -17,7 +17,8 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -130,7 +131,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT=BASE_DIR / 'staticfiles'
 STATICFILES_DIRS=[BASE_DIR / 'WebShopX' / 'static',]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR/ 'media'#STATIC_URL = "static/"
 
