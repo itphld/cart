ddcb362ce5e21f8386184049834b413e386a7e9c
itphld <itplanet12@gmail.com>
2025-08-14 12:14:00 +0530

    Fix: increased max_length for email/username/mobile

--- account/models.py
+++ account/models.py
@@ -32,7 +32,7 @@ class Account(AbstractBaseUser):
     mobile=models.CharField(max_length=20)
     join_date=models.DateTimeField(auto_now_add=True)
     last_login=models.DateTimeField(auto_now_add=True)
-    password=models.CharField(max_length=50)
+    password=models.CharField(max_length=100)
 
     #required
     is_admin=models.BooleanField(default=True)
